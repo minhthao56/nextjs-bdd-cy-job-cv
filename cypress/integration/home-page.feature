aeb2676aebe805227a3e3a5e7dbe4d4665e7ae98
Nguyen Minh Thao <minhthao5648@gmail.com>
2021-09-09 17:32:33 +0700

    add material ui

--- cypress/integration/home-page.feature
+++ cypress/integration/home-page.feature
@@ -2,4 +2,4 @@ Feature: Goto Home Page
 
     Scenario: Test Home Page
         Given User Open Home Page
-        Then  User See "Welcome to Next.js!"
+        Then  User See "Home Page"
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// pages/_app.tsx
+/* eslint-disable react/jsx-props-no-spreading */
+import { FC, useEffect } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { AppProps } from "next/app";
+import Head from "next/head";
+import theme from "@styles/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>My App</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+};
+
+export default MyApp;
